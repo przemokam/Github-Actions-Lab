9a54a34912823bd51c6da75af873053d8a4f9605
PrzemoKam <przemek.kamola@gmail.com>
2026-03-05 14:41:34 +0100

    changing Stripe secret

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const config = {
     connectionString: 'postgresql://admin:SuperSecret123!@db.example.com:5432/prod'
   },
   stripe: {
-    secretKey: 'sk_live_51234567890abcdefghijklmnop'
+    secretKey: 'sk_live_51234567890abcdefghijklmnoP'
   },
   jwt: {
     secret: 'my-super-secret-jwt-key-do-not-share'
